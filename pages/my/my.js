162840ef7e1a50de7e778c913bb18e80297dfb95
fight1991 <359193813@qq.com>
2021-06-01 17:55:37 +0800

    我的页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,11 +1,13 @@
 // pages/my/my.js
+var app = getApp()
+const { logOut } = app.api
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    wxUserImg: app.static_user_logo
   },
 
   /**
@@ -14,7 +16,30 @@ Page({
   onLoad: function (options) {
 
   },
-
+  // 用户退出
+  loginOut () {
+    wx.showModal({
+      title: '温馨提示',
+      content: '您确定要退出登录吗?',
+      success: async (res) => {
+        if (res.confirm) {
+          let { result } = await logOut()
+          if (result) {
+            wx.showToast({
+              title: '退出成功!',
+              duration: 1500,
+              success: () => {
+                wx.reLaunch({
+                  url: '/pages/login/signIn',
+                })
+                wx.removeStorageSync('token')
+              }
+            })
+          }
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
