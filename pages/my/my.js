797a4571ddf2a97ab703d0d7bbe39b010fc79a27
fight1991 <359193813@qq.com>
2021-06-30 11:12:51 +0800

    暂存

--- pages/my/my.js
+++ pages/my/my.js
@@ -11,6 +11,27 @@ Page({
     authPersonal: false, // 是否已实名
     authVehicleLicense: false, // 是否行驶证认证
     userName: '',
+    tabList: [{
+      url: '/pages/card/card',
+      label: '我的银行卡',
+      icon: 'card-finance',
+      color: '#ffa44b'
+    }, {
+      url: '/pages/ticket/ticket',
+      label: '我的卡券',
+      icon: 'ticket',
+      color: '#447fff'
+    }, {
+      url: '/pages/message/messageList',
+      label: '我的消息',
+      icon: 'info',
+      color: '#0dd7be'
+    }, {
+      url: '/pages/carInfo/carList',
+      label: '我的车辆',
+      icon: 'car',
+      color: '#769dff'
+    }]
   },
 
   /**
