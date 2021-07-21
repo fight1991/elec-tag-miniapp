5c4118cc5557d8ce1c0052471df9e66c68813edf
fight1991 <359193813@qq.com>
2021-07-21 17:04:49 +0800

    商圈首页添加

--- pages/circle/circle.js
+++ pages/circle/circle.js
@@ -5,7 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    gridIcon: [{
+      id: 'id1',
+      label: '无感加油',
+      icon: '/pages/image/businessCircle/oil.png'
+    }, {
+      id: 'id2',
+      label: '维修保养',
+      icon: '/pages/image/businessCircle/maintenance.png'
+    }, {
+      id: 'id3',
+      label: '洗车美容',
+      icon: '/pages/image/businessCircle/wash.png'
+    }, {
+      id: 'id4',
+      label: '智慧停车',
+      icon: '/pages/image/businessCircle/parking.png'
+    }, {
+      id: 'id5',
+      label: '保险理赔',
+      icon: '/pages/image/businessCircle/insurance.png'
+    }, {
+      id: 'id6',
+      label: '道路救援',
+      icon: '/pages/image/businessCircle/rescue.png'
+    }, {
+      id: 'id7',
+      label: '违章处理',
+      icon: '/pages/image/businessCircle/violation.png'
+    }, {
+      id: 'id8',
+      label: '汽车租赁',
+      icon: '/pages/image/businessCircle/rental.png'
+    }, {
+      id: 'id9',
+      label: '轮胎维护',
+      icon: '/pages/image/businessCircle/tire.png'
+    }, {
+      id: 'id10',
+      label: '车辆置换',
+      icon: '/pages/image/businessCircle/replace.png'
+    }, {
+      id: 'id11',
+      label: '年检服务',
+      icon: '/pages/image/businessCircle/annual.png'
+    }, {
+      id: 'id12',
+      label: '敬请期待',
+      icon: '/pages/image/businessCircle/more.png'
+    }]
   },
 
   /**
