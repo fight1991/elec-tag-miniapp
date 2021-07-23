c0498bfa2cd0bdbd5618ed8374c82ccfcb409566
fight1991 <359193813@qq.com>
2021-07-23 15:46:47 +0800

    暂存

---
+++ pages/components/outerTag/outerTag.js
@@ -0,0 +1,29 @@
+// pages/components/outerTag/outerTag.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  options: {
+    multipleSlots: true
+  },
+  properties: {
+    active: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
