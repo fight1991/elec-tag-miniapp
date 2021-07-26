667c0a3c252f525831c687737a0f37cd4531e46f
fight1991 <359193813@qq.com>
2021-07-26 17:48:42 +0800

    暂存

---
+++ pages/subPages/parking/parkingRecord.js
@@ -0,0 +1,101 @@
+// pages/subPages/parking/parkingRecord.js
+var app = getApp()
+const { listApi } = app.api
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hasMore: true,
+    pageIndex: 0, // 当前页
+    pageSize: 10, // 每页请求数量
+    total: 0, // 条目数
+    loading: false, // 正在加载
+    resultList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // this.initList()
+  },
+  // 获取列表
+  async getList (pageIndex, callback) {
+    if (this.loading) return
+    this.loading = true
+    let { pageSize, resultList } = this.data
+    let createTime = ''
+    let id = ''
+    if (resultList.length > 0) {
+      var lastData = resultList[resultList.length - 1]
+      createTime = lastData.createTime
+      id = lastData.id
+    }
+    pageIndex ++
+    let { result, page } = await listApi({
+      data: {
+        createTime,
+        id
+      },
+      page: {
+        pageIndex,
+        pageSize
+      }
+    })
+    if (result) {
+      callback && callback(result || [], page)
+    }
+    this.loading = false
+  },
+  // 列表初始化查询
+  initList () {
+    this.getList(0, (resList, pagination) => {
+      var { pageIndex, total, pageSize } = pagination
+      this.setData({
+        pageIndex,
+        list: resList,
+        hasMore: pageIndex * pageSize >= total ? false : true
+      })
+      wx.stopPullDownRefresh()
+    })
+  },
+  onPullDownRefresh: function () {
+    this.initList()
+    // 停止下拉刷新
+    // wx.stopPullDownRefresh()
+  },
+  onReachBottom: function () {
+    if (!this.data.hasMore) return
+    let { pageIndex, list } = this.data
+    this.getList(pageIndex, (resList, pagination) => {
+      var { pageIndex, total, pageSize } = pagination
+      this.setData({
+        pageIndex,
+        list: [...list, ...resList],
+        hasMore: pageIndex * pageSize >= total ? false : true
+      })
+    })
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  }
+})
