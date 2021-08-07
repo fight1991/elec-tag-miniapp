d1a7becf69a0f5e0fb918c58d8cc746d6018db25
fight1991 <359193813@qq.com>
2021-08-07 11:31:55 +0800

    暂存

--- config/index.js
+++ config/index.js
@@ -7,5 +7,8 @@ export default {
   BUSINESS_API: 'https://test-ec-api.htjicon.com', // 业务相关api
   assetsHost: 'https://test-oss.htjicon.com' // 静态图片的域名
   // 生产
-  
+  // FILE: 'https://common-api.htjicon.com', // 文件上传 // 公共平台
+  // USRE_API: 'https://common-api.htjicon.com', // 字典 // 公共平台
+  // BUSINESS_API: 'https://ec-api.htjicon.com', // 业务相关api
+  // assetsHost: 'https://oss.htjicon.com' // 静态图片的域名
 }
