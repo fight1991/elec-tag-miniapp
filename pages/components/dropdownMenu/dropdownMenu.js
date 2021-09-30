153c8229508374c42d7b9dab6752b6985388ccaf
fight1991 <359193813@qq.com>
2021-09-30 17:54:25 +0800

    自定义下拉暂存

---
+++ pages/components/dropdownMenu/dropdownMenu.js
@@ -0,0 +1,91 @@
+// pages/components/dropdownMenu/dropdownMenu.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  options: {
+    addGlobalClass: true
+  },
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    height: 0,
+    tabValue: {
+      distance: '3km',
+      oil: '92#',
+      other: '距离最近'
+    },
+    showMask: false,
+    currentTabName: 'distance', // 当前选择的tab
+    tabs: { // tab渲染项
+      distance: [
+        {label: '3km', value: 3},
+        {label: '5km', value: 5},
+        {label: '10km', value: 10},
+        {label: '15km', value: 15},
+        {label: '不限', value: 'none'}
+      ],
+      oil: [
+        {label: '92#', value: 92},
+        {label: '95#', value: 95},
+        {label: '98#', value: 98},
+        {label: '0#', value: 0}
+      ],
+      other: [
+        {label: '距离最近', value: 'near'},
+        {label: '价格最低', value: 'low'},
+      ]
+    }
+  },
+  lifetimes: {
+    attached: function () {
+      let that = this
+      const query = this.createSelectorQuery()
+      query.select('#select-box').boundingClientRect()
+      query.exec(function(res){
+        console.log(res)
+        that.setData({
+          height: res[0].bottom
+        })
+      })
+    }
+  },
+  pageLifetimes: {
+   
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 筛选条件按钮
+    selectBtn (e) {
+      let currentTab = e.target.dataset.tab
+      this.setData({
+        showMask: true,
+        currentTabName: currentTab
+      })
+    },
+    // 下方筛选项 
+    tagTap (e) {
+      let index = e.currentTarget.id
+      console.log(index)
+      let { currentTabName: currTab, tabs } = this.data
+      this.setData({
+        ['tabValue.' + [currTab]]: tabs[currTab][index]['label'],
+        showMask: false
+      })
+    },
+    // 点击模态框
+    maskTap () {
+      this.setData({
+        showMask: false
+      })
+    },
+    itemsTap () {},
+  }
+})
