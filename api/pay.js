0b598b3b23dc473511e9e046e9b2a835282e6f32
fight1991 <359193813@qq.com>
2021-11-15 15:38:53 +0800

    支付相关api

--- api/pay.js
+++ api/pay.js
@@ -7,3 +7,25 @@ export const getPayCouponList = ({ data, page }) => {
     page
   })
 }
+
+// 确认支付
+export const addPay = (data) => {
+  return wx.$post_business({
+    url: '/user-center/trade/addPay',
+    data,
+  })
+}
+// 获取账户是否冻结
+export const getFreezeStatus = (data) => {
+  return wx.$post_business({
+    url: '/user-center/trade/getFreezeStatus',
+    data,
+  })
+}
+// 锁定优惠券
+export const lockCoupon = (data) => {
+  return wx.$post_business({
+    url: '/user-center/trade/lockCoupon',
+    data,
+  })
+}
